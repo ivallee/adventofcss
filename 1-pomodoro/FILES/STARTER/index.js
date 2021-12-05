a4a158b197a2d1c51be35adb01875dc2db12805d
ivallee <isaacvallee@gmail.com>
2021-12-05 09:36:07 -0800

    init commit + challenge 1

---
+++ 1-pomodoro/FILES/STARTER/index.js
@@ -0,0 +1,11 @@
+const circle = document.querySelector('.timer__ring-circle');
+const radius = circle.r.baseVal.value;
+const circumference = radius * 2 * Math.PI;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = `${circumference}`;
+
+function setProgress(percent) {
+  const offset = circumference - percent / 100 * circumference;
+  return offset;
+}
