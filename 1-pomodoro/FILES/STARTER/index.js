060029b0d5e729ed5ea096e0d3f909055c938d93
Isaac Vallee <i.vallee@astoundcommerce.com>
2021-12-12 17:15:38 -0800

    Base timer functions

--- 1-pomodoro/FILES/STARTER/index.js
+++ 1-pomodoro/FILES/STARTER/index.js
@@ -1,3 +1,4 @@
+const timer = document.querySelector('.timer__input');
 const circle = document.querySelector('.timer__ring-circle');
 const radius = circle.r.baseVal.value;
 const circumference = radius * 2 * Math.PI;
@@ -9,3 +10,26 @@ function setProgress(percent) {
   const offset = circumference - percent / 100 * circumference;
   return offset;
 }
+
+function updateCountdown(endDate, range) {
+  const diff = Math.max(0, endDate - new Date());
+
+  const percentComplete = Math.floor(100 - 100 * diff / range);
+  console.log(percentComplete)
+  circle.style.strokeDashoffset = setProgress(percentComplete);
+  // TODO: Update timer input
+  
+  if (diff >= 0) {
+    requestAnimationFrame(() => updateCountdown(endDate, range));
+  }  
+}
+
+function startTimer(input) {
+  const startDate = new Date();
+  // TODO: make endDate dynamic based on timer input
+  const endDate = new Date(startDate.getTime() + 10000); // + 10 seconds
+  const range = endDate - startDate;
+  updateCountdown(endDate, range);
+}
+
+startTimer();
